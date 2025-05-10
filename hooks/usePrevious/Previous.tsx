a28dc906bb195f474afa9753b27190770d5c9b27
badasswp <badasswpdev@gmail.com>
2025-05-10 15:12:15 +0100

    chore: update hook components

--- hooks/usePrevious/Previous.tsx
+++ hooks/usePrevious/Previous.tsx
@@ -1,4 +1,5 @@
-import React, { useState, JSX } from 'react';
+import { useState } from 'react';
+import type { JSX } from 'react';
 import usePrevious from './';
 
 /**
--- hooks/useToggle/Toggle.tsx
+++ hooks/useToggle/Toggle.tsx
@@ -1,4 +1,5 @@
-import React, { JSX } from 'react';
+import type { JSX } from 'react';
+
 import useToggle from './';
 
 interface ToggleProps {
