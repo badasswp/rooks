2c17c074094ec00329647ad286179cfba47d3a6f
badasswp <badasswpdev@gmail.com>
2025-05-11 14:15:11 +0100

    test: setup test config

---
+++ jest.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+};
